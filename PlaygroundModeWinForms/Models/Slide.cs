e1b15f9e2fc6ae331f0dfed1d1437637c31e5fc4
Danila <kdv0807@gmail.com>
2020-07-21 20:51:18 +0700

    Тест формул распределения

--- PlaygroundModeWinForms/Models/Slide.cs
+++ PlaygroundModeWinForms/Models/Slide.cs
@@ -10,7 +10,19 @@ namespace PlaygroundModeWinForms.Models
     {
         public override double DistributionFunction(double x)
         {
-            throw new NotImplementedException();
+            if (x >= 0 && x < 2)
+            {
+                return 1.1 * Math.Pow(Math.E, (-2 * x + 2)) - 0.1;
+            }
+            else if (x >= 2 && x < 9.5)
+            {
+                return Math.Pow(Math.E, (-Math.Pow((x - 7), 2) / (2 * 0.2))) / (0.28 * 2 * Math.Sqrt(Math.PI));
+            }
+            else if (x >= 9.5 && x < 17)
+            {
+                return Math.Pow(Math.E, (-Math.Pow((x - 12.5), 2) / (2 * 0.5))) / (0.25 * 2 * Math.Sqrt(Math.PI));
+            }
+            return -1;
         }
 
         public override string GetInfo()
---
+++ PlaygroundModeWinForms/Views/TestForm.cs
@@ -0,0 +1,63 @@
+﻿using PlaygroundModeWinForms.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PlaygroundModeWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Test();
+        }
+
+        private void Test()
+        {
+            TestPersonDistributionFunction();
+            TestRockingSpringDistributionFunction();
+            TestSlideDistributionFunction();
+        }
+
+        private void TestPersonDistributionFunction()
+        {
+            var name = "Человек";
+            var obj = new Person();
+            chart3.Series[0].IsVisibleInLegend = false;
+            chart3.Series.Add(name);
+            chart3.Series[name].ChartType = SeriesChartType.Line;
+            chart3.Series[name].Color = Color.Red;
+            for (double i = 1; i < 17; i += 0.001) chart3.Series[name].Points.AddXY(i, obj.DistributionFunction(i));
+        }
+
+        private void TestRockingSpringDistributionFunction()
+        {
+            var name = "Пружина";
+            var obj = new RockingSpring();
+            chart2.Series[0].IsVisibleInLegend = false;
+            chart2.Series.Add(name);
+            chart2.Series[name].ChartType = SeriesChartType.Line;
+            chart2.Series[name].Color = Color.Red;
+            for (double i = 1; i < 17; i += 0.001) chart2.Series[name].Points.AddXY(i, obj.DistributionFunction(i));
+        }
+
+        private void TestSlideDistributionFunction()
+        {
+            var name = "Горка";
+            var obj = new Slide();
+            chart1.Series[0].IsVisibleInLegend = false;
+            chart1.Series.Add(name);
+            chart1.Series[name].ChartType = SeriesChartType.Line;
+            chart1.Series[name].Color = Color.Red;
+            for (double i = 1; i < 17; i += 0.001) chart1.Series[name].Points.AddXY(i, obj.DistributionFunction(i));
+        }
+    }
+}
